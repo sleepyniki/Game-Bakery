2c809f2ff1dbdf2d3858f54dfbd1710029ce5a42
niki middel <nickmiddel2000685@gmail.com>
2025-10-27 12:04:26 +0100

    v4
    added jump

--- Assets/playerhandeler.cs
+++ Assets/playerhandeler.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class playerhandeler : MonoBehaviour
 {
     Rigidbody rb;
+    public Rigidbody Rb;
     [SerializeField] private float playerspeed;
 
 
@@ -11,11 +12,19 @@ public class playerhandeler : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
     }
-    
-    private void FixedUpdate() 
+
+    private void FixedUpdate()
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         rb.MovePosition(rb.position + movement * playerspeed * Time.fixedDeltaTime);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * 300);
+        }
     }
 }
 
