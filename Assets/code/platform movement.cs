54a2dfcdf3b341499b2132bff0872f8bb9ba8fb5
niki middel <nickmiddel2000685@gmail.com>
2025-10-31 12:29:18 +0100

    V9
    moving platform and waypoints added

---
+++ Assets/code/platform movement.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class platformmovement : MonoBehaviour
+{
+    public float speed;
+    public int startingpoint;
+    public Transform[] points;
+    private int i;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.position = points[startingpoint].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, points[i].position) < 0.02f)
+        {
+           i++;
+           if(i == points.Length) 
+           {
+             i = 0;
+           }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.transform.SetParent(transform);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        collision.transform.SetParent(null);
+    }
+}
