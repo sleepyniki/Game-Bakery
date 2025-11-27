14ce9b2afc9ceb8ea872ba653bd6d50b602bb4be
niki middel <nickmiddel2000685@gmail.com>
2025-11-27 10:46:08 +0100

    v27
    added a win

---
+++ Assets/code/Win.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Win : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            Debug.Log("you won!");
+        }
+    }
+}
